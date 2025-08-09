29c3ac835df55c5c1bf8f8331cf1c8d79cd080a2
Pranto <jhpranto53@gmail.com>
2025-08-09 21:50:35 +0600

    chore: setup db and trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div className="text-rose-500">Hello World</div>;
+import { caller } from "@/trpc/server";
+
+export default async function Home() {
+  const data = await caller.hello({ text: "SERVER world" });
+
+  return <div className="text-rose-500 text-3xl">{data.greeting}</div>;
 }
